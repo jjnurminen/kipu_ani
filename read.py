314551884ff7a3b04e40440a69ec513176d378be
Jussi Nurminen <jnu@iki.fi>
2016-06-02 14:20:53 +0300

    works ok

--- read.py
+++ read.py
@@ -16,7 +16,7 @@ import datetime
 import dateutil
 
 
-DATA_PATH = "C:/Users/HUS20664877/projects/kipu_ani/data"
+DATA_PATH = "data"
 
 
 def not_a_number(str):
@@ -26,26 +26,48 @@ def not_a_number(str):
     except ValueError:
         return True
     return False
-   
+
+def is_time(timestr):
+    """ Check for valid time string, i.e. hh:mm:ss """
+    tli = timestr.split(':')
+    if len(tli) != 3:
+        return False
+    try:
+        hms = [int(s) for s in tli]
+    except ValueError:
+        return False
+    if hms[0] in range(24) and hms[1] in range(60) and hms[2] in range(60):
+        return True
+    else:
+        return False
+
 def procline(li):
     """ Takes one line, returns tuple:
     (time, events, energy, ani, animean, quality)
-    or None if line cannot be parsed """
+    or None if line cannot be parsed into the variables """
     # this is to work around irregularly formatted lines:
-    # item separator can be either 2 or more whitespace chars OR a single tab    
+    # item separator can be either 2 or more whitespace chars OR a single tab
+    # (not a single space, since event strings may contain a space...)
     lis = re.split('\s\s+|\t', li)
     lis = [s.strip() for s in lis]
-    #print(lis)
-    if not len(lis) in [6,7]:
+    if not len(lis) in [6,7]:   # weird result of split, cannot parse
         return None
     time = lis[0]
-    # check for time string
-    if not (len(time) == 8 and time[2] == ':' and time[5] == ':'):
-        return None
-    if len(lis) == 7:  # we have an event
+    # check for validity of time string
+    if not is_time(time):
+        if len(time) > 8:  # time and event glued together?
+            if is_time(time[:8]):  
+                print('warning: overlong time string, splitting into time and event:', time)
+                time = time[:8]
+                event = time[8:]
+            else:   # overlong time string, cannot parse
+                return None
+        else:   # normal or short time string, but cannot parse
+            return None
+    if len(lis) == 7:  # valid time followed by event
         events = lis[1].strip()
         lis.pop(1)
-    else:
+    else:  # len == 6, no event
         events = None
     energy = float(lis[1])
     ani = int(lis[2])
@@ -53,15 +75,17 @@ def procline(li):
     quality = float(lis[4])
     if events:
         assert not_a_number(events)
+        events = events.upper()
     assert ani >= 0 and ani <= 100
     assert animean >= 0 and animean <= 100    
     return (time, events, energy, ani, animean, quality)
 
 def ani_array(lines):
-    """ Makes a numpy array from ani file,
+    """ Makes a numpy array from ani output lines,
     with columns: time (s) (from start of file), energy, ani, animean, quality.
     returns index of experiment start time + the array """
-    ar = None
+    EXP_START = ['ALKU', 'ALOITUS']  # magic strings indicating experiment start
+    ar = []
     t0 = None
     parsed_a_line = False
     t_exp_start = None
@@ -75,14 +99,11 @@ def ani_array(lines):
             if not parsed_a_line:
                 t0 = timep   # file start time
             # indicates experiment start (hand immersion)
-            if events == 'ALKU' or events == 'ALOITUS':
+            if events in EXP_START:
                 t_exp_start = (timep-t0).seconds
             t = (timep - t0).seconds
             vals = [t, energy, ani, animean, quality]
-            if ar is not None:            
-                ar = np.vstack((ar, vals))
-            else:
-                ar = np.array(vals)
+            ar.append(vals)
             parsed_a_line = True
         else:
             # unparseable line in middle of file?
@@ -93,56 +114,66 @@ def ani_array(lines):
     if not t_exp_start:
         raise ValueError('Cannot parse t_exp_start')
     # experiment start in seconds (hand in water) relative to file start
-    return t_exp_start, ar
+    nar = np.array(ar)
+    if not np.all(np.diff(nar[:,0]) == 1):
+        print('warning: time not uniformly sampled, delta t stats: ', end='')
+        print(np.unique(np.diff(nar[:,0]), return_counts=True))
+    return t_exp_start, nar
 
-T_BEFORE = 20  # sec before t0
-T_AFTER = 160  # sec after t0
-EXCLUDE_ZEROS = True  # exclude curves that fall to zero (errors?)
+def ani_plot():
+    T_BEFORE = 20  # sec before t0
+    T_AFTER = 160  # sec after t0
+    EXCLUDE_ZEROS = True  # exclude curves that fall to zero (errors?)
+    MAX_LINES = 1000  # do not read more data than this
+    
+    """ Plot t vs ANI curves """
+    plt.figure()
+    parse_errors = 0
+    files = glob.glob(DATA_PATH + '/' + '*txt')
+    nfiles = len(files)
+    avg = np.zeros(T_BEFORE+T_AFTER)
+    navg = 0
+    tvec = None
+    n_curves_zero = 0 
 
-""" Plot t vs ANI curves """
-plt.figure()
-parse_errors = 0
-files = glob.glob(DATA_PATH + '/' + '*txt')
-nfiles = len(files)
-avg = np.zeros(T_BEFORE+T_AFTER)
-navg = 0
-n_curves_zero = 0 
-for fn in files:
-    print('Parsing:', fn)
-    f = open(fn, 'r')
-    lines = f.readlines()
-    try:
-        t_start_ind, ar1 = ani_array(lines)        
-    except ValueError as e:
-        print(e)
-        parse_errors += 1
-        continue
-    tvec = ar1[t_start_ind-T_BEFORE:t_start_ind+T_AFTER, 0]
-    tvec -= tvec[0]  # plot starts from zero
-    ani_data = ar1[t_start_ind-T_BEFORE:t_start_ind+T_AFTER, 2]
-    # do not plot or include in average, if curve falls to zero
-    if min(ani_data) == 0:
-        n_curves_zero += 1
-        if EXCLUDE_ZEROS:
+    for fn in files[:-1]:
+        print('Parsing:', fn)
+        f = open(fn, 'r')
+        lines = f.readlines()[:MAX_LINES]
+        try:
+            t_start_ind, ar1 = ani_array(lines)        
+        except ValueError as e:
+            print(e)
+            parse_errors += 1
             continue
-    avg += ani_data
-    navg += 1
-    plt.plot(tvec, ani_data)
-    plt.draw()
-    #plt.waitforbuttonpress()
-
-avg /= navg
-plt.plot(tvec, avg, linewidth=3, color='k')
-plt.axvline(T_BEFORE, linestyle='--', color='k')
-plt.xlabel('Time (s)')
-plt.ylabel('ANI index')
-print('\nTotal files:', nfiles)
-print('Files with parse errors:', parse_errors)
-print('Curves with ANI falling to zero:', n_curves_zero)
-print('Plotted curves:', navg)
+        tvec = ar1[t_start_ind-T_BEFORE:t_start_ind+T_AFTER, 0]
+        tvec -= tvec[0]  # plot starts from zero
+        ani_data = ar1[t_start_ind-T_BEFORE:t_start_ind+T_AFTER, 2]
+        # do not plot or include in average, if curve falls to zero
+        if min(ani_data) == 0:
+            n_curves_zero += 1
+            if EXCLUDE_ZEROS:
+                continue
+        avg += ani_data
+        navg += 1
+        plt.plot(tvec, ani_data)
+        plt.draw()
+        #plt.waitforbuttonpress()
+    
+    avg /= navg
+    plt.plot(tvec, avg, linewidth=3, color='k')
+    plt.axvline(T_BEFORE, linestyle='--', color='k')
+    plt.xlabel('Time (s)')
+    plt.ylabel('ANI index')
+    print('\nTotal files found:', nfiles)
+    print('Files with parse errors:', parse_errors)
+    print('Curves with ANI falling to zero:', n_curves_zero)
+    print('Plotted curves:', navg)
+   
+if __name__ == '__main__':
+    ani_plot()
     
 
 
+        
     
-    
-
